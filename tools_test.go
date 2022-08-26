82a60758ad89fcd88e02598407d57886e4c8787d
Youngjae Lim <limyoungjae@gmail.com>
2022-08-25 22:13:02 -0400

    Add a test for DownloadStaticFile

--- tools_test.go
+++ tools_test.go
@@ -5,7 +5,9 @@ import (
 	"image"
 	"image/png"
 	"io"
+	"io/ioutil"
 	"mime/multipart"
+	"net/http"
 	"net/http/httptest"
 	"os"
 	"sync"
@@ -231,5 +233,30 @@ func TestTools_Slugify(t *testing.T) {
 		if err == nil && e.errorExpected {
 			t.Errorf("%s: error expected when none receivded: %s", e.name, err.Error())
 		}
+	}
+}
+
+func TestTools_DownloadStaticFile(t *testing.T) {
+	rr := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+
+	var testTool Tools
+
+	testTool.DownloadStaticFile(rr, req, "./testdata", "pic.jpg", "downloaded_pic.jpg")
+
+	res := rr.Result()
+	defer res.Body.Close()
+
+	if res.Header["Content-Length"][0] != "98827" {
+		t.Error("wrong content length of", res.Header["Content-Length"][0])
+	}
+
+	if res.Header["Content-Disposition"][0] != "attachment; filename=\"downloaded_pic.jpg\"" {
+		t.Error("wrong content dispostion")
+	}
+
+	_, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Error(err)
 	}
 }
